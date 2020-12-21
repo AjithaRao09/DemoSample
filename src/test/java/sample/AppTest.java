d14a5f2fea0064e871b8fd818368aa39d842b44f
AjithaRao09 <ajitha.n2507@gmail.com>
2020-12-21 10:37:55 -0500

    Commit the changes

--- src/test/java/sample/AppTest.java
+++ src/test/java/sample/AppTest.java
@@ -6,27 +6,28 @@ import org.testng.annotations.Test;
 
 public class AppTest {
 	
-	   @ Test
+	   @ Test(groups = {"Modulue1"})
 	   public void maincall()
 	   {
 	           System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");  
 	           //IE,Firefox,Chrome,safari
+	           System.out.println("Modulue1");
 	           WebDriver driver=new ChromeDriver(); 
 	           driver.navigate().to("https://www.phptravels.net/home"); 
 	           driver.manage().window().maximize();
 	   }
 	      
+	   @ Test(groups= {"Modulue 2"})
+	   public void mainnncall()
+	   {
+	           System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");  
+	           //IE,Firefox,Chrome,safari
+	           System.out.println("modulue2");
+	           WebDriver driver=new ChromeDriver(); 
+	           driver.navigate().to("https://www.phptravels.net/home"); 
+	           driver.manage().window().maximize();
+	   }
 	      
-	   //   @Test
-	      public void maincalll()
-	      {
-	   	   System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");  
-	          //IE,Firefox,Chrome,safari
-	          WebDriver driver=new ChromeDriver(); 
-	          driver.navigate().to("https://www.phptravels.net/home"); 
-	          driver.manage().window().maximize();
-	   	   
-	      }
-	      
+
 
 }
